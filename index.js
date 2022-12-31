eae34ce2b099cfbb71bff2d39dde9826cf76180b
Kush <kushpatel124@gmail.com>
2022-12-30 20:00:11 -0500

    commit tree 984614cb75b5b5090d19037fe7f1f43b86e25ed6

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = ({
+    name: name,
+    email: email,
+  });
+  console.log(formData)
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+      document.body.innerHTML = object.id
+    })
+    .catch(function (error) {
+      console.log(error);
+      document.body.innerHTML = error.message
+    })
+}
+
